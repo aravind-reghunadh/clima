05c534872034514da53cc1f24ed5b62c5ea25bd0
aravind-reghunadh <aravind_reghunadh@ieee.org>
2021-05-31 23:42:28 +0530

    added the geolocater

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -8,10 +8,11 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
 
     print(position);
+    print('cliked');
   }
 
   @override
@@ -19,7 +20,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {},
+          onPressed: () {
+            getLocation();
+          },
           child: Text('Get Location'),
         ),
       ),
