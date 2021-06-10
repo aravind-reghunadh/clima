1ecfd7fe80e73aab7aa4534f949e4d2f278d8505
aravind-reghunadh <aravind_reghunadh@ieee.org>
2021-06-11 00:12:43 +0530

    made few changes in getdata

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,8 +21,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b1b15e88fa797225412429c1c50c122a1');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var longitude = jsonDecode(data)['coord']['lon'];
+      var weatherdescription = jsonDecode(data)['weather'][0]['description'];
+      var temperature = jsonDecode(data)['main']['temp'];
+      var condition = jsonDecode(data)['weather'][0]['id'];
+      var cityName = jsonDecode(data)['weather'][0]['id'];
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
